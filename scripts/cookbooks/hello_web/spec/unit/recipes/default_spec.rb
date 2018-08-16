de94c1146b52aa5bc009632c21e8019d4c0492bc
Joaquin Menchaca <suavecali@yahoo.com>
2018-08-16 16:51:13 +0900

    ChefSpec/ServerRunner - 100% coverage spec

--- scripts/cookbooks/hello_web/spec/unit/recipes/default_spec.rb
+++ scripts/cookbooks/hello_web/spec/unit/recipes/default_spec.rb
@@ -2,42 +2,78 @@
 # Cookbook:: hello_web
 # Spec:: default
 #
-# Copyright:: 2018, The Authors, All Rights Reserved.
+# Copyright:: 2018, Joaquín Menchaca, All Rights Reserved.
 
 require 'spec_helper'
 
 describe 'hello_web::default' do
   context 'When all attributes are default, on Ubuntu 16.04' do
+    let(:package) { 'apache2' }
+    let(:service) { 'apache2' }
+    let(:docroot) { '/var/www/html' }
+
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04') do |node|
-        node.normal['hello_web']['package'] = 'apache2'
-        node.normal['hello_web']['service'] = 'apache2'
-        node.normal['hello_web']['docroot'] = '/var/www/html'
-      end
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'Should Updates the Apt Cache Daily' do
+      expect(chef_run).to periodic_apt_update 'Update the apt cache daily'
+    end
+
+    it 'Should Installs Apache Web Server Package' do
+      expect(chef_run).to install_package package
+    end
+
+    it 'Should enables Apache Web Server service' do
+      expect(chef_run).to enable_service service
+    end
+
+    it 'Should starts Apache Web Server service' do
+      expect(chef_run).to start_service service
+    end
+    
+    it 'Should copy HTML content to docroot' do
+      expect(chef_run).to create_cookbook_file "#{docroot}/index.html"
     end
   end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
+  context 'When overriding attirubtes on 7.5.1804' do
+    let(:package) { 'httpd' }
+    let(:service) { 'httpd' }
+    let(:docroot) { '/var/www/html' }
+
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708') do |node|
-        node.normal['hello_web']['package'] = 'httpd'
-        node.normal['hello_web']['service'] = 'httpd'
-        node.normal['hello_web']['docroot'] = '/var/www/html'
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.5.1804') do |node|
+        node.normal['hello_web']['package'] = package
+        node.normal['hello_web']['service'] = package
+        node.normal['hello_web']['docroot'] = docroot
       end
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'Should Installs Apache Web Server Package' do
+      expect(chef_run).to install_package package
+    end
+
+    it 'Should enables Apache Web Server service' do
+      expect(chef_run).to enable_service service
+    end
+
+    it 'Should starts Apache Web Server service' do
+      expect(chef_run).to start_service service
+    end
+
+    it 'Should copy HTML content to docroot' do
+      expect(chef_run).to create_cookbook_file "#{docroot}/index.html"
     end
   end
 end
