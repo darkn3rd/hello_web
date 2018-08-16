11876f64f81c4fe1920e59de264d2d8257142cd8
Joaquin Menchaca <suavecali@yahoo.com>
2018-08-16 16:10:42 +0900

    chefspec/serverrunner - initial convergence

--- scripts/cookbooks/hello_web/spec/unit/recipes/default_spec.rb
+++ scripts/cookbooks/hello_web/spec/unit/recipes/default_spec.rb
@@ -11,7 +11,11 @@ describe 'hello_web::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04') do |node|
+        node.normal['hello_web']['package'] = 'apache2'
+        node.normal['hello_web']['service'] = 'apache2'
+        node.normal['hello_web']['docroot'] = '/var/www/html'
+      end
       runner.converge(described_recipe)
     end
 
@@ -24,7 +28,11 @@ describe 'hello_web::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708') do |node|
+        node.normal['hello_web']['package'] = 'httpd'
+        node.normal['hello_web']['service'] = 'httpd'
+        node.normal['hello_web']['docroot'] = '/var/www/html'
+      end
       runner.converge(described_recipe)
     end
 
