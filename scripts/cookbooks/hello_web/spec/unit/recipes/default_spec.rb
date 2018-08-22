3670add49f75e8f624943160a89a7c74a849d1a4
Joaquin Menchaca <suavecali@yahoo.com>
2018-08-21 22:57:39 -0700

    wip: trying out new renderer w/o success

--- scripts/cookbooks/hello_web/spec/unit/recipes/default_spec.rb
+++ scripts/cookbooks/hello_web/spec/unit/recipes/default_spec.rb
@@ -7,73 +7,75 @@
 require 'spec_helper'
 
 describe 'hello_web::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
+  #platform 'ubuntu'
+
+  context 'When all attributes are default, on Ubuntu' do
     let(:package) { 'apache2' }
     let(:service) { 'apache2' }
     let(:docroot) { '/var/www/html' }
 
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
+    # let(:chef_run) do
+    #   runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
+    #   runner.converge(described_recipe)
+    # end
 
     it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+      is_expected.to_not raise_error
     end
 
     it 'Should Updates the Apt Cache Daily' do
-      expect(chef_run).to periodic_apt_update 'Update the apt cache daily'
+      is_expected.to periodic_apt_update 'Update the apt cache daily'
     end
 
     it 'Should Installs Apache Web Server Package' do
-      expect(chef_run).to install_package package
+      is_expected.to install_package package
     end
 
     it 'Should enables Apache Web Server service' do
-      expect(chef_run).to enable_service service
+      is_expected.to enable_service service
     end
 
     it 'Should starts Apache Web Server service' do
-      expect(chef_run).to start_service service
+      is_expected.to start_service service
     end
 
     it 'Should copy HTML content to docroot' do
-      expect(chef_run).to create_cookbook_file "#{docroot}/index.html"
+      is_expected.to create_cookbook_file "#{docroot}/index.html"
     end
   end
 
-  context 'When overriding attirubtes on 7.5.1804' do
-    let(:package) { 'httpd' }
-    let(:service) { 'httpd' }
-    let(:docroot) { '/var/www/html' }
-
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'centos', version: '7.5.1804') do |node|
-        node.normal['hello_web']['package'] = package
-        node.normal['hello_web']['service'] = package
-        node.normal['hello_web']['docroot'] = docroot
-      end
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-
-    it 'Should Installs Apache Web Server Package' do
-      expect(chef_run).to install_package package
-    end
-
-    it 'Should enables Apache Web Server service' do
-      expect(chef_run).to enable_service service
-    end
-
-    it 'Should starts Apache Web Server service' do
-      expect(chef_run).to start_service service
-    end
-
-    it 'Should copy HTML content to docroot' do
-      expect(chef_run).to create_cookbook_file "#{docroot}/index.html"
-    end
-  end
+  # context 'When overriding attirubtes on 7.5.1804' do
+  #   let(:package) { 'httpd' }
+  #   let(:service) { 'httpd' }
+  #   let(:docroot) { '/var/www/html' }
+  #
+  #   let(:chef_run) do
+  #     runner = ChefSpec::SoloRunner.new(platform: 'centos', version: '7.5.1804') do |node|
+  #       node.normal['hello_web']['package'] = package
+  #       node.normal['hello_web']['service'] = package
+  #       node.normal['hello_web']['docroot'] = docroot
+  #     end
+  #     runner.converge(described_recipe)
+  #   end
+  #
+  #   it 'converges successfully' do
+  #     expect { chef_run }.to_not raise_error
+  #   end
+  #
+  #   it 'Should Installs Apache Web Server Package' do
+  #     expect(chef_run).to install_package package
+  #   end
+  #
+  #   it 'Should enables Apache Web Server service' do
+  #     expect(chef_run).to enable_service service
+  #   end
+  #
+  #   it 'Should starts Apache Web Server service' do
+  #     expect(chef_run).to start_service service
+  #   end
+  #
+  #   it 'Should copy HTML content to docroot' do
+  #     expect(chef_run).to create_cookbook_file "#{docroot}/index.html"
+  #   end
+  # end
 end
