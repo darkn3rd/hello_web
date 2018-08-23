4b19d04fac589f6b4004d6067030eb1415ed8cb3
Joaquin Menchaca <suavecali@yahoo.com>
2018-08-23 20:28:44 +0900

    Chef + Kitchen + Inspec integration tests

--- scripts/cookbooks/hello_web/test/integration/default/default_test.rb
+++ scripts/cookbooks/hello_web/test/integration/default/default_test.rb
@@ -5,14 +5,27 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+PACKAGE = attribute('package', default: 'apache2', description: 'package name')
+SERVICE = attribute('service', default: 'apache2', description: 'service name')
+
+######## USELESS TESTS - DONT DO THIS ########
+describe package(PACKAGE) do
+  it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe service(SERVICE) do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+######## CONTRACT TESTS ########
+describe port(80) do
+  it { should be_listening }
+end
+
+describe http('localhost') do
+  its('status') { should eq 200 }
+  its('headers.Content-Type') { should include 'text/html' }
+  its('body') { should include 'Hello World!' }
 end
