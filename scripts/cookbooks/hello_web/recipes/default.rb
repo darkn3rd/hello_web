62606770a4708a1693ff090a588e13b9371f969f
Joaquin Menchaca <suavecali@yahoo.com>
2018-08-26 08:45:51 +0900

    chef cookbook reference attributes by platform

--- scripts/cookbooks/hello_web/recipes/default.rb
+++ scripts/cookbooks/hello_web/recipes/default.rb
@@ -4,19 +4,24 @@
 #
 # Copyright:: 2018, Joaquin Menchaca, All Rights Reserved.
 
+# reference these locally for readibility
+package = node['hello_web']["#{node['platform_family']}"]['package']
+service = node['hello_web']["#{node['platform_family']}"]['service']
+docroot = node['hello_web']["#{node['platform_family']}"]['docroot']
+
 apt_update 'Update the apt cache daily' do
   frequency 86_400
   action :periodic
 end
 
-package node['hello_web']['package']
+package package
 
-cookbook_file "#{node['hello_web']['docroot']}/index.html" do
+cookbook_file "#{docroot}/index.html" do
   source 'index.html'
   action :create
 end
 
-service node['hello_web']['service'] do
+service service do
   supports status: true, restart: true, reload: true
   action %i(enable start)
 end
