5a672d64398d857aa72bcec05c905455fcbdbb22
Joaquin Menchaca <suavecali@yahoo.com>
2018-08-25 21:46:54 +0900

    chef cookbook hello_web with dynamic attributes based on platform_faily using case/when clause

--- scripts/cookbooks/hello_web/attributes/default.rb
+++ scripts/cookbooks/hello_web/attributes/default.rb
@@ -1,3 +1,20 @@
-default['hello_web']['package'] = 'apache2'
-default['hello_web']['docroot'] = '/var/www/html'
-default['hello_web']['service'] = 'apache2'
+
+# Dynamic Attributes based on Platform Family
+case node['platform_family']
+when 'rhel'
+  default['hello_web']['package'] = 'httpd'
+  default['hello_web']['service'] = 'httpd'
+  default['hello_web']['docroot'] = '/var/www/html'
+when 'debian'
+  default['hello_web']['package'] = 'apache2'
+  default['hello_web']['service'] = 'apache2'
+  default['hello_web']['docroot'] = '/var/www/html'
+when 'freebsd'
+  default['hello_web']['package'] = 'apache24'
+  default['hello_web']['docroot'] = '/usr/local/www/apache24/data/'
+  default['hello_web']['service'] = 'apache24'
+else
+  default['hello_web']['package'] = nil
+  default['hello_web']['docroot'] = nil
+  default['hello_web']['service'] = nil
+end
--- scripts/cookbooks/hello_web/test/integration/default/default_test.rb
+++ scripts/cookbooks/hello_web/test/integration/default/default_test.rb
@@ -5,24 +5,6 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-PACKAGE = attribute('package', default: 'apache2', description: 'package name')
-SERVICE = attribute('service', default: 'apache2', description: 'service name')
-
-######## ILLUSTRATIVE PURPOSES ONLY ########
-# NOTE: These tests are put here for teaching purposes, to illustrate basic
-#  functionality of InSpec. These tests have low or negative value, as they
-#  they don't test the promise of the service, but implementation, which varies
-#  greatly from operating system to operating system.
-describe package(PACKAGE) do
-  it { should be_installed }
-end
-
-describe service(SERVICE) do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
-end
-
 ######## CONTRACT TESTS ########
 # NOTE: These tests test the promise or contract of the service, which is that
 # that there is a listening web server with 'Hellow World!' content.
